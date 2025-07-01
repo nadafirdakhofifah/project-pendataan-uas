a398c8771a44074356dcaa6bc45cca87f8f8e767
Nada Firda Khofifah <143942532+nadafirdakhofifah@users.noreply.github.com>
2025-07-01 15:00:44 +0700

    Add files via upload

---
+++ GURU.sql
@@ -0,0 +1,25 @@
+DESCRIBE Guru;
+
+INSERT INTO Guru (NUPTK, Nama, Kode_Mapel) 
+VALUES
+('001345689012', 'Ahmad Setiawan, S.Pd.', 'MAT001'),
+('023405789013', 'Budi Santoso, M.Pd.', 'BIO002'),
+('078456789014', 'Citra Wulandari, S.Pd.', 'KIM003'),
+('098456789015', 'Dian Lestari, M.Pd.', 'FIS004'),
+('099456789016', 'Eka Prasetya, S.Pd.', 'IND005'),
+('023456789017', 'Fajar Nugroho, M.Pd.', 'SEJ006'),
+('097456789018', 'Gita Ayu Lestari, S.Pd.', 'GEO007'),
+('087456789019', 'Heri Susanto, M.Pd.', 'SOS008'),
+('043456789020', 'Indra Wijaya, S.Pd.', 'PKN009'),
+('005645689021', 'Joko Purnomo, M.Pd.', 'SEN010'),
+('067456789022', 'Kartika Sari, S.Pd.', 'PEN011'),
+('037456789023', 'Lestari Dewi, M.Pd.', 'TIK012'),
+('054456789024', 'Maya Sari, S.Pd.', 'ING013'),
+('077456789025', 'Nurdin Maulana, M.Pd.', 'JRM014'),
+('079456789026', 'Oki Prasetyo, S.Pd.', 'BIO015'),
+('027456789027', 'Putri Ayu, M.Pd.', 'MAT016'),
+('029456789028', 'Rahmawati, S.Pd.', 'KIM017'),
+('082456789029', 'Satria Dharma, M.Pd.', 'FIS018'),
+('072456789030', 'Taufik Hidayat, S.Pd.', 'IND019'),
+('198456789031', 'Wulan Anggraini, M.Pd.', 'SEJ020');
+SELECT * FROM Guru;
